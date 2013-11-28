a7056ae8cb439f05a94c8b5f46f890cdf390ed07
kenu <kenu.heo@gmail.com>
2013-11-28 09:07:19 +0900

    SimpleActivity

---
+++ SimpleActivity/src/kr/kenuheo/simpleactivity/SubActivity.java
@@ -0,0 +1,33 @@
+package kr.kenuheo.simpleactivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sub);
+		
+		Button subBtn = (Button)findViewById(R.id.subBtn);
+		subBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sub, menu);
+		return true;
+	}
+
+}
